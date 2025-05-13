3d8a56ce7de687fba98c3e35350f8de83f7a378e
alelodato <alss.ldt@gmail.com>
2025-05-13 16:48:42 +0200

    adjusted comments files code to pass validation

--- comments/models.py
+++ comments/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from posts.models import Post
 
+
 class Comment(models.Model):
     """
     Comment model, related to User and Post
@@ -9,9 +10,11 @@ class Comment(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at= models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
     content = models.TextField()
+
     class Meta:
         ordering = ['-created_at']
+
     def __str__(self):
         return self.content
