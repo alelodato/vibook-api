ec57d3dc400d6eb2865288b8d0c1b0635158f569
alelodato <alss.ldt@gmail.com>
2025-05-18 11:07:02 +0200

    fixed image source in contact serializer

--- contact/serializers.py
+++ contact/serializers.py
@@ -10,7 +10,7 @@ class ContactSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(
-        source='owner.profile.profile_pic.url'
+        source='owner.profile.image.url'
     )
 
     def get_is_owner(self, obj):
